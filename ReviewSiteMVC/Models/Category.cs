8c3bf0bb3eade107454e36320d7633487e3b7f84
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-07-14 23:36:44 -0400

    first commit

---
+++ ReviewSiteMVC/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Destination { get; set; }
+        public string Accommodations { get; set; }
+        public string Transportation { get; set; }
+        public string Activities { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
